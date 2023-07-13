ca7950a2f5ca77596aa3bcedc3dd696f0a82806f
softroniics-calicut <softroniicscalicut@gmail.com>
2023-07-13 11:49:30 +0530

    second

---
+++ lib/functions.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+
+void main(){
+  print('Enter first number: ');
+  int a=int.parse(stdin.readLineSync()!);
+
+  print('Enter second number :');
+  int b=int.parse(stdin.readLineSync()!);
+
+  int c=multiply(a,b);
+  print(c);
+}
+int multiply(d,e){
+  int multiply=d*e;
+  return multiply;
+}
+
+
+
