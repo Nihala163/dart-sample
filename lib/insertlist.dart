f608c5c1e4f0ac58d79c01a5c004c8ae6d49effa
softroniics-calicut <softroniicscalicut@gmail.com>
2023-07-14 11:29:49 +0530

    first commit 2

--- lib/insertlist.dart
+++ lib/insertlist.dart
@@ -1,28 +1,28 @@
 import 'dart:io';
 
 void main() {
-  var list1 = [];
+  var list2 = [];
 
   print("Enter a number : ");
   for (int a = 1; a <= 5; a++) {
     int d = int.parse(stdin.readLineSync()!);
-    list1.add(d);
+    list2.add(d);
   }
-  print(list1);
+  print(list2);
 
   print('enter starting range :');
   int c = int.parse(stdin.readLineSync()!);
   print('enter ending range :');
   int d = int.parse(stdin.readLineSync()!);
-  list1.removeRange(c, d);
+  list2.removeRange(c, d);
 
-  print(list1);
+  print(list2);
 
 
   // print('insert number :');
   // int e = int.parse(stdin.readLineSync()!);
-  list1.insertAll(1,[1,2,3]);
-  print(list1);
+  list2.insertAll(1,[1,2,3]);
+  print(list2);
 
 
 }
